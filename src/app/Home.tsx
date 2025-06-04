3759ad918953e1624ba5956312ec3de3e3752c3b
Jaylo <jaylogalam04@gmail.com>
2025-06-04 22:03:24 +0800

    new content

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -2,11 +2,13 @@ import Text from "@/components/text";
 
 function Home() {
   return (
-    <div className="">
-      <Text variant="hero"> Code. Design. Iterate. Repeat. </Text>
-      <Text variant="title">
-        I’m Jaylo, and I craft digital experiences that feel as good as
-        they look.
+    <div className="mt-40 p-12 h-screen w-[70%]">
+      <Text variant="hero"> Hey, I'm Jaylo! </Text>
+      <Text variant="body" className="w-[60%]">
+        I'm studying Computer Science and exploring Full-Stack Development and
+        AI, continuesly learning to improve my skills. I build applications that
+        solve real-life problems and enjoy experimenting with new technologies,
+        tools, and ideas to push my capabilities further.
       </Text>
     </div>
   );
